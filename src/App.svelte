e75f8cb46406bd4d3d0bba88c497598b7d0816f8
Andreas Göth <a@hrhr.se>
2019-10-08 21:05:17 +0200

    Update styles a bit

--- src/App.svelte
+++ src/App.svelte
@@ -53,27 +53,48 @@
 </script>
 
 <style>
-  body {
-    background: #F9F9FA;
+
+  :root {
+    --main-bg-color: #f9f9fa;
+    --main-text-color: #0C0C0D;
+
+    --main-max-width: 1042px;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --main-bg-color: #2a2a2e;
+      --main-text-color: #f9f9fa;
+    }
+  }
+
+  :global(body) {
+    background: var(--main-bg-color);
+    color: var(--main-text-color);
+  }
+
+  .header {
+    max-width: var(--main-max-width);
+    margin: 3rem auto 2rem;
+    font-size: 1.2rem;
   }
 
   .bookmarks-container {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
+    grid-gap: 20px;
+    grid-template-columns: repeat( auto-fill, minmax(100px, 1fr) );
     grid-auto-rows: minmax(100px, auto);
+    max-width: var(--main-max-width);
+    margin: 3rem auto 2rem auto;
   }
 
   .bookmarks-item {
     text-align: center;
-  }
-
-  .bookmarks-item:hover .bookmarks-item-icon {
-    box-shadow: 0 0 0 5px #ddd;
+    cursor: pointer;
   }
 
   .bookmarks-item-icon {
-    border-radius: 5px;
+    border-radius: 4px;
     background: #ccc;
     box-shadow: 0 1px 4px 0 rgba(12, 12, 13, 0.1);
     width: 100px;
@@ -84,12 +105,17 @@
   .bookmarks-item-name {
     font-size: 0.8rem;
     margin-top: 0.3rem;
-    color: #333;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
+
 </style>
 
 {#if bookmarks !== null}
-  <h1>{getFolderName(bookmarks)}</h1>
+
+  <div class="header">{getFolderName(bookmarks)}</div>
+
   <div class="bookmarks-container">
     {#each bookmarks.children as bookmark}
       <div class="bookmarks-item" on:click={event => onBookmarkClick(event, bookmark)}>
@@ -105,3 +131,4 @@
     {/each}
   </div>
 {/if}
+
