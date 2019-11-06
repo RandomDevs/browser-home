93a8fbce637ff0c12984e0f8b1cb26015e0fd4df
Andreas Göth <a@hrhr.se>
2019-11-06 09:55:48 +0100

    Change folder select to a simple dropdown

--- src/ui/ChooseBookmarkFolder.svelte
+++ src/ui/ChooseBookmarkFolder.svelte
@@ -2,33 +2,43 @@
   import { onMount } from 'svelte'
   import { getCurrentFolderId } from './store'
   import { getAllBookmarkFolders, setBookmarkFolderId } from './utils/browser'
-  import FolderOption from './FolderOption.svelte'
 
   let folderId
+  let bookmarkOptions = []
+
+  function flattenBookmarkTree(tree, indent=0) {
+    return tree.reduce((acc, item) => {
+      const itemArr = [{ id: item.id, title: item.title, indent }]
+      return (item.children.length === 0)
+        ? acc.concat(itemArr)
+        : acc.concat(itemArr, flattenBookmarkTree(item.children, indent + 1))
+    }, [])
+  }
 
   onMount(async () => {
     folderId = await getCurrentFolderId()
+    const bookmarkTree = await getAllBookmarkFolders()
+    bookmarkOptions = flattenBookmarkTree(bookmarkTree.children)
   })
 
-  function chooseFolder(id) {
-    folderId = id
-  }
-
-  async function save() {
-    await setBookmarkFolderId(folderId)
-
-    test = folderId
+  async function save(event) {
+    await setBookmarkFolderId(event.target.value)
   }
 </script>
 
 <style>
+  .label {
+    font-size: 1rem;
+    margin-bottom: 4px;
+  }
 </style>
 
-{#await getAllBookmarkFolders() then folderTree }
-  <ul>
-    {#each folderTree.children as folder}
-      <FolderOption folder={folder} onClick={chooseFolder} chosenFolderId={folderId} />
-    {/each}
-  </ul>
-  <button on:click={save}>Save</button>
-{/await}
+<label for="select-home-folder" class="label">Home folder</label>
+<select {folderId} class="select" id="select-home-folder" on:change={save} bind:value={folderId}>
+{#each bookmarkOptions as option }
+  <option value={option.id} selected={option.id === folderId}>
+  {'     '.repeat(option.indent)}📁 {option.title}
+  </option>
+{/each}
+</select>
+
