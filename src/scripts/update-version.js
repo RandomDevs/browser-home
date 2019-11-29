b8ee90931f3e318037631236839f6e61d13019d9
Simon Östergren <simon.ostergren@gmail.com>
2019-11-29 23:53:56 +0100

    Fix version in manifest when building.

---
+++ src/scripts/update-version.js
@@ -0,0 +1,36 @@
+const fs = require('fs').promises
+
+function parseVersion(version) {
+  return version.replace('refs.tags.', '')
+}
+
+async function run(version) {
+
+  const manifestFilePath = './public/manifest.json'
+
+  // Check if output directory exists
+  try {
+    await fs.access(manifestFilePath)
+  } catch (error) {
+    console.error('Error: Manifest file is not accessable')
+    process.exit(1)
+  }
+
+  const content = await fs.readFile(manifestFilePath, { encoding: 'utf-8' })
+  const json = JSON.parse(content)
+
+  json.version = parseVersion(version)
+
+  const dataToStore = JSON.stringify(json, null, 2)
+  await fs.writeFile(manifestFilePath, dataToStore, { encoding: 'utf-8' })
+
+  console.log(`Updated version in manifest to ${parseVersion(version)}`)
+}
+
+if (process.argv[2] === undefined) {
+
+  console.error('Usage: node version.js [version]')
+  process.exit(1)
+}
+
+run(process.argv[2])
