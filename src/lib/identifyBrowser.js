c412f7a6696a6c5c6f3ce6986748990fd0c58f9b
Simon Östergren <simon.ostergren@gmail.com>
2020-02-18 21:40:40 +0100

    Support for edge browser

--- src/lib/identifyBrowser.js
+++ src/lib/identifyBrowser.js
@@ -1,10 +1,15 @@
 
 const TYPE_CHROME = 'chrome'
+const TYPE_EDGE = 'edge'
 const TYPE_FIREFOX = 'firefox'
 
 function identifyBrowser() {
 
   const { userAgent } = navigator
+
+  if (/Edg\//.test(userAgent)) {
+    return TYPE_EDGE
+  }
 
   if (/Chrome/.test(userAgent)) {
     return TYPE_CHROME
