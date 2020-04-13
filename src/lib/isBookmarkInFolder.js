10a14fa593043806c560f8af319437a51d6ce5b7
Simon Östergren <simon.ostergren@gmail.com>
2020-04-13 21:28:57 +0200

    Handle bookmark types that is not supported by Chrome. (#48)

--- src/lib/isBookmarkInFolder.js
+++ src/lib/isBookmarkInFolder.js
@@ -1,12 +1,15 @@
+const { getBookmarkType, TYPE_BOOKMARK, TYPE_FOLDER } = require('./bookmarkHelpers')
+
 function searchBookmarksTreeForId(tree, bookmarkId) {
 
   for (const bookmark of tree) {
 
     let searchResult
+    const type = getBookmarkType(bookmark)
 
-    if (bookmark.children) {
+    if (type === TYPE_FOLDER) {
       searchResult = searchBookmarksTreeForId(bookmark.children, bookmarkId)
-    } else {
+    } else if (type === TYPE_BOOKMARK) {
       searchResult = bookmark.id === bookmarkId
     }
 
